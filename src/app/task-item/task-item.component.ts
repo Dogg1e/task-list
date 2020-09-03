6a982fe892d158a853e80b3fdeebd26e60202c98
Antipov-AS <asantipov@sberbank.ru>
2020-09-03 12:43:27 +0500

    add component

---
+++ src/app/task-item/task-item.component.ts
@@ -0,0 +1,22 @@
+import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import {Task} from '../app.component';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.scss']
+})
+export class TaskItemComponent implements OnInit {
+
+  @Input() task: Task;
+  @Output() onEdit = new EventEmitter<string>();
+
+  ngOnInit(): void {
+  }
+
+  edit(id: any): void {
+    this.onEdit.emit(id);
+    console.log(id);
+  }
+
+}
